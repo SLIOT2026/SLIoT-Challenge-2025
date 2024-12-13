a7d797cf3f37b3a5bebb4bf2f3bd33ebad36569c
iransamarasekara <iransamarasekara@gmail.com>
2024-12-14 00:35:53 +0530

    add gallery, about

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,15 @@
+import Gallery from "@/components/Gallery";
+import About from "../components/About";
+import Spotlight from "@/components/Spotlight";
+
 const Home = () => {
-  return <div className="w-full"></div>;
+  return (
+    <div className="w-full">
+      <About />
+      <Gallery />
+      <Spotlight />
+    </div>
+  );
 };
 
 export default Home;
