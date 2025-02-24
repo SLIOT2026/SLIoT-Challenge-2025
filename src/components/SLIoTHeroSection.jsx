ed3838ddcab09e84fd91934f927a810a657f18af
Chameera KHD <dinith.22@cse.mrt.ac.lk>
2025-02-24 18:13:15 +0530

    Selected Teams

--- src/components/SLIoTHeroSection.jsx
+++ src/components/SLIoTHeroSection.jsx
@@ -4,7 +4,9 @@ import { robot, sltLogo } from "@/assets";
 import {motion} from "framer-motion"
 import { Spotlight } from "./ui/Spotlight";
 import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+import { useNavigate } from "react-router-dom";
 const SLIoTHeroSection = () => {
+  const navigate = useNavigate();
   const [showSpotlights, setShowSpotlights] = useState(false);
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -71,11 +73,11 @@ const SLIoTHeroSection = () => {
                   className="relative inline-block"
                 >
                   <div className="absolute inset-0 translate-x-1 translate-y-1 border-2 border-gray-400 lg:translate-y-2 lg:translate-x-2 rounded-xl"></div>
-                  <a href="https://forms.gle/MD9WLWwD2fPFLCPY8" target='_blank' className="z-20">
-                    <button className="relative z-10 px-4 py-2 text-base text-white transition duration-300 shadow-lg sm:px-8 lg:px-4 lg:py-3 lg:text-lg font-nicoMoji rounded-xl bg-gradient-to-r from-pink-500 to-purple-600">
-                      Submit Proposal
+                  {/* <a href="" target='_blank' className="z-20"> */}
+                    <button onClick={() => navigate("/selected-teams")} className="relative z-10 px-4 py-2 text-base text-white transition duration-300 shadow-lg sm:px-8 lg:px-4 lg:py-3 lg:text-lg font-nicoMoji rounded-xl bg-gradient-to-r from-pink-500 to-purple-600">
+                      Selected Teams
                     </button>
-                  </a>
+                  {/* </a> */}
                 </div>
               </motion.div>
               <div className="relative flex items-end justify-center gap-6 mt-6 md:justify-end md:mt-10 lg:-mt-10 md:gap-10">
